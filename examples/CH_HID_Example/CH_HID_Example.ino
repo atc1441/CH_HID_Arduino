f79dfc20afbb8eed7ee3b4be446662ae099e3b2a
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-07-06 12:23:53 +0200

    Added more examples

--- examples/CH_HID_Example/CH_HID_Example.ino
+++ examples/CH_HID_Example/CH_HID_Example.ino
@@ -1,20 +1,26 @@
 #include "CH_HID_Arduino.h"
 
 void setup() {
-	
+
   ch_hid.begin();
-  
+
   Keyboard.print("Hallo");
-  
-  //Mouse.click(MOUSE_LEFT);  
-  //Mouse.move(0, -40);
-  
+  delay(1000);
+  Keyboard.press(KEY_LEFT_ARROW);
+  Keyboard.release(KEY_LEFT_ARROW);
+  //Keyboard.releaseAll();
+
+  //Mouse.click(MOUSE_LEFT); // Click Mouse button
+  //Mouse.move(0, -40); // Move mouse X Y
+  //Mouse.move(0, 0, 3); // Mouse wheel plus 3
+  //Mouse.move(0, 0, -3); // Mouse wheel minus 3
+
   //ch_hid.reset();
   //ch_hid.bootloader();
-  
+
 }
 
 
 void loop() {
-	//nothing to do here.
+  //nothing to do here.
 }
